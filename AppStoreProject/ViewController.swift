048bc361754d4fd6cc474ef8dfa7e6dfa1b2bfc4
Joy <kimjoydev@gmail.com>
2024-08-08 23:46:26 +0900

    Initial Commit

---
+++ AppStoreProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreProject
+//
+//  Created by Joy Kim on 8/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
